286b54d1543e91ea4cde9f9820b9416f8298800b
Poria Ebrahimi <poria.ebrahimi123@gmail.com>
2019-08-30 16:13:31 -0500

    Title Screen basic start and end button implemented (#19)

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public void start_button_scene()
+    {
+        Application.LoadLevel("Map1");
+    }
+
+    public void end_button_scene()
+    {
+        Application.Quit();
+    }
+}
