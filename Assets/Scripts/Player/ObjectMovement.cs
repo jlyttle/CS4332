0b6d677a0fcb099fe6b5e937c2ab04fa87cbcd4e
ChooJosh <33579646+ChooJosh@users.noreply.github.com>
2019-08-31 18:38:26 -0500

    Completed basic unit movement script (#23)
    * Added collideable to mountains, water, elevated terrain. TODO: allow bridges to be walkable
    
    * implemented collider for terrain, water, and mountains. TODO: allow for walking through bridge.
    
    * add 53755.png
    
    * added basic player movement script. BUG: player object gets stuck on collideable terrain. TODO: Add maximum movement space unit can go.
    
    * added fighter unit object prefab to test
    
    * modified map1
    
    * Added collideable to mountains, water, elevated terrain. TODO: allow bridges to be walkable
    
    * add 53755.png
    
    * added basic player movement script. BUG: player object gets stuck on collideable terrain. TODO: Add maximum movement space unit can go.
    
    * added fighter unit object prefab to test
    
    * modified map1

---
+++ Assets/Scripts/Player/ObjectMovement.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//set directions
+public enum DIRECTION { UP, DOWN, LEFT, RIGHT }
+
+public class ObjectMovement : MonoBehaviour
+{
+	private bool canMove = true, moving = false;
+	private int speed = 5, buttonCooldown = 0; //button cooldown for not moving too many directions at once
+	private DIRECTION dir = DIRECTION.DOWN; //facing direction									
+	private Vector3 pos; //keep track of object position
+
+	public void Update()
+	{
+		buttonCooldown--;
+		if (canMove)
+		{
+			pos = transform.position;
+			move();
+		}
+
+        if (moving)
+		{
+            //if current position is equivalent to the position you are trying to reach
+            if (transform.position == pos)
+			{
+				moving = false;
+				canMove = true;
+				move();
+			}
+            //movement speed 
+			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
+		}
+	}
+
+	public void move()
+	{
+		if (buttonCooldown <= 0)
+		{
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				if (dir != DIRECTION.UP)
+				{
+					buttonCooldown = 2;
+					dir = DIRECTION.UP;
+				}
+				else
+				{
+					canMove = false;
+					moving = true;
+					pos += Vector3.up;
+				}
+			}
+            else if (Input.GetKey(KeyCode.DownArrow))
+			{
+				if (dir != DIRECTION.DOWN)
+				{
+					buttonCooldown = 2;
+					dir = DIRECTION.DOWN;
+				}
+				else
+				{
+					canMove = false;
+					moving = true;
+					pos += Vector3.down;
+				}
+			}
+			else if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				if (dir != DIRECTION.LEFT)
+				{
+					buttonCooldown = 2;
+					dir = DIRECTION.LEFT;
+				}
+				else
+				{
+					canMove = false;
+					moving = true;
+					pos += Vector3.left;
+				}
+			}
+			else if (Input.GetKey(KeyCode.RightArrow))
+			{
+				if (dir != DIRECTION.RIGHT)
+				{
+					buttonCooldown = 2;
+					dir = DIRECTION.RIGHT;
+				}
+				else
+				{
+					canMove = false;
+					moving = true;
+					pos += Vector3.right;
+				}
+			}
+		}
+	}
+}
