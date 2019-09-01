c34464450c30033284a8e3ba068cd040107149b2
PoriaEbrahimi <poria.ebrahimy@gmail.com>
2019-08-31 21:09:36 -0500

    added resume and exit buttons with functionalities.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject pause_menu;
+    public static bool is_paused = false;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (is_paused == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pause_menu.SetActive(false);
+        Time.timeScale = 1f;
+        is_paused = false;
+    }
+
+    public void Save()
+    {
         
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Pause()
+    {
+        pause_menu.SetActive(true);
+        Time.timeScale = 0f;
+        is_paused = true;
     }
 }
