033899d7ab04dd8d11dc6b3065bc7a24551d8230
Jonathan Lyttle <myncijub@gmail.com>
2019-09-23 20:11:24 -0500

    Keep track of current tile coordinates for cursor

--- Assets/Scripts/Player/ObjectMovement.cs
+++ Assets/Scripts/Player/ObjectMovement.cs
@@ -7,17 +7,26 @@ public enum DIRECTION { UP, DOWN, LEFT, RIGHT }
 
 public class ObjectMovement : MonoBehaviour
 {
+    public Grid grid;
 	private bool canMove = true, moving = false;
 	private int speed = 5, buttonCooldown = 0; //button cooldown for not moving too many directions at once
 	private DIRECTION dir = DIRECTION.DOWN; //facing direction									
 	private Vector3 pos; //keep track of object position
+    private Vector3Int currentPosition;
 
 	private bool isWalkable = false;
 	private int range = 5;
 
-	public void Update()
+    public void Start()
+    {
+        currentPosition = grid.WorldToCell(transform.position);
+    }
+
+    public void Update()
 	{
-		buttonCooldown--;
+        Vector2 screenBoundaries = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        buttonCooldown--;
 		if (canMove)
 		{
 			pos = transform.position;
@@ -36,7 +45,7 @@ public class ObjectMovement : MonoBehaviour
             //movement speed 
 			transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);
 		}
-	}
+    }
 
 	public void move()
 	{
